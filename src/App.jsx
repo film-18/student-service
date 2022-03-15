57f249fa42357f582a49951f407150a8ab763b7f
film-kanlaya <56312158+film-18@users.noreply.github.com>
2022-03-15 17:07:34 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import './App.css'
+
+import { Button, DatePicker } from 'antd';
+import { Layout, Menu, Breadcrumb } from 'antd';
+
+
+function App() {
+  const { Header, Content, Footer } = Layout;
+
+  return (
+    <div className="App">
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            {new Array(15).fill(null).map((_, index) => {
+              const key = index + 1;
+              return <Menu.Item key={key}>{`nav ${key}`}</Menu.Item>;
+            })}
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-content">Content</div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      </Layout>
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { getThemeVariables } from 'antd/dist/theme';
+import vitePluginImp from 'vite-plugin-imp';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    vitePluginImp({
+      libList: [
+        {
+          libName: 'antd',
+          style: (name) => `antd/es/${name}/style`,
+        },
+      ],
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        modifyVars: {
+          // ...getThemeVariables({
+          //   dark: true
+          // }),
+          '@primary-color': '#5F98A9',
+        },
+        // modifyVars: getThemeVariables({
+        //   '@primary-color': '#01B636'
+        // }),
+        javascriptEnabled: true,
+      },
+    },
+  },
+})
