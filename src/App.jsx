260b790cf71dac56b3462ba3279c1ee6ae16a613
Kanlaya Pootsupha <62070011@kmitl.ac.th>
2022-05-10 19:38:13 +0700

    Change antd and moment.js to Thai locale

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import './App.css'
-import './styles/service.css'
+import { useState } from "react";
+import "./App.css";
+import "./styles/service.css";
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Navbar from './components/Navbar';
-import { Home } from './pages/Home';
-import { Service } from './pages/Service';
-import { Notification } from './pages/Notification';
-import { Profile } from './pages/Profile';
-import { RequestOne } from './pages/RequestOne';
+import { ConfigProvider } from "antd";
+import Navbar from "./components/Navbar";
+import { Home } from "./pages/Home";
+import { Service } from "./pages/Service";
+import { Notification } from "./pages/Notification";
+import { Profile } from "./pages/Profile";
+import { RequestOne } from "./pages/RequestOne";
 
+import Thai from "antd/lib/locale/th_TH";
 
+import moment from 'moment'
+import 'moment/locale/th'
+moment.locale('th')
 
 function App() {
-
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Navbar />}>
-        <Route index element={<Home />} />
-        <Route path="service/" element={<Service />} />
-        <Route path="notification/" element={<Notification />} />
-        <Route path="profile/" element={<Profile />} />
-        <Route path="service/:requestOne" element={<RequestOne />} />
-      
-      </Route>
-    </Routes>
-    </BrowserRouter>
+    <ConfigProvider locale={Thai}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />}>
+            <Route index element={<Home />} />
+            <Route path="service/" element={<Service />} />
+            <Route path="notification/" element={<Notification />} />
+            <Route path="profile/" element={<Profile />} />
+            <Route path="service/:requestOne" element={<RequestOne />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ConfigProvider>
   );
 }
 
