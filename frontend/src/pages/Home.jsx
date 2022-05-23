696efb531748c16ba4e03eddc703e15660cab848
Yannawut62070051 <56510625+Yannawut62070051@users.noreply.github.com>
2022-05-24 00:01:05 +0700

    query staff to render

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,46 @@
 import { memo, useEffect, useState } from "react";
-import { Image, Carousel, Row, Col, Button, Typography, Layout, Pagination } from 'antd';
-import { AlertFilled, ExclamationCircleFilled } from '@ant-design/icons';
-import data from "../data/news.json"
+import {
+  Image,
+  Carousel,
+  Row,
+  Col,
+  Button,
+  Typography,
+  Layout,
+  Pagination,
+} from "antd";
+import { AlertFilled, ExclamationCircleFilled } from "@ant-design/icons";
+import data from "../data/news.json";
 import { NewsItem } from "../components/NewsItem";
 import { CalendarItem } from "../components/CalendarItem";
-import axios from 'axios'
-import { Avatar } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
-
+import axios from "axios";
+import { Avatar } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import { gql, useQuery, useMutation } from "@apollo/client";
 
 function onChange(a, b, c) {
-    console.log(a, b, c);
+  console.log(a, b, c);
 }
+
+const queryStaff = gql`
+  query {
+    users(filter: { role: staff }) {
+      fullname
+      firstname
+      lastname
+      username
+      password
+      email
+      year
+      role
+      degree
+      program
+      major
+      _id
+      imageUri
+    }
+  }
+`;
 
 // const contentStyle = {
 //     height: '500px',
@@ -21,21 +50,20 @@ function onChange(a, b, c) {
 //     background: '#364d79',
 // };
 
-
 export const Home = memo(() => {
+  const [news, setNews] = useState(null);
+  const { data: staff, refetch } = useQuery(queryStaff);
 
-    const [news, setNews] = useState(null);
+  useEffect(() => {
+    axios.get("http://localhost:8000/news").then((res) => {
+      console.log(res.data);
+      setNews(res.data);
+    });
+  }, []);
 
-    useEffect(() => {
-        axios.get('http://localhost:8000/news')
-            .then((res) => {
-                console.log(res.data);
-                setNews(res.data)
-            })
-    }, [])
-
-    return <>
-        {/* <Carousel afterChange={onChange}>
+  return (
+    <>
+      {/* <Carousel afterChange={onChange}>
             <div>
                 <h3 style={contentStyle}>
                     <img src="https://www.it.kmitl.ac.th/wp-content/themes/itkmitl2017wp/img/itkmitl-panorama-1.jpg" style={{ width: "100%", height: "500px", objectFit: "cover"}}></img>
@@ -47,128 +75,116 @@ export const Home = memo(() => {
 
         </Carousel> */}
 
-        <Layout style={{ backgroundColor: "#112B3C" }}>
-            <div className="container">
-                <div className="row">
-                    <div className="col-12 col-md-6 text-start my-5">
-                        <Typography.Title level={3} style={{ color: "#08979c" }} mark>
-                            ล่าสุด !
-                            {/* <ExclamationCircleFilled /> */}
-                        </Typography.Title>
-                        <h4 style={{ color: "#08979c" }}>
-                            Undergraduate Admission – TCAS Round 5 Academic Year 2019
-                        </h4>
-                        <p style={{ color: "#08979c" }}>
-                            Faculty of Information Technology announces the list of qualified applicants to be interview on Thursday, June 13, 2019 at Faculty of Information Technology Building, 1st Floor, Room M04, KMITL.
-                        </p>
-                        <Button type="primary" shape="round" size="large" onClick={() => location.href = '/news'}>
-                            <span >
-                                อ่านเพิ่มเติม
-                            </span>
-                        </Button>
-
-                    </div>
-                    <div className="col-12 col-md-6">
-
-                        <div className="news-img">
-                            <img src="https://www.it.kmitl.ac.th/wp-content/uploads/2019/06/1300x867_BSC_TCAS-5-2562-ANNOUCEMENT-OF-QUALIFIED-CANDIDATES-FOR-INTERVIEW.jpg" width="100%" />
-                        </div>
-
-                    </div>
-
-                </div>
+      <Layout style={{ backgroundColor: "#112B3C" }}>
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-6 text-start my-5">
+              <Typography.Title level={3} style={{ color: "#08979c" }} mark>
+                ล่าสุด !{/* <ExclamationCircleFilled /> */}
+              </Typography.Title>
+              <h4 style={{ color: "#08979c" }}>
+                Undergraduate Admission – TCAS Round 5 Academic Year 2019
+              </h4>
+              <p style={{ color: "#08979c" }}>
+                Faculty of Information Technology announces the list of
+                qualified applicants to be interview on Thursday, June 13, 2019
+                at Faculty of Information Technology Building, 1st Floor, Room
+                M04, KMITL.
+              </p>
+              <Button
+                type="primary"
+                shape="round"
+                size="large"
+                onClick={() => (location.href = "/news")}
+              >
+                <span>อ่านเพิ่มเติม</span>
+              </Button>
             </div>
-
-        </Layout>
-
-
-        <div className="container my-5">
-            <Typography.Title level={2}>
-                ข่าวสารและประชาสัมพันธ์
-            </Typography.Title>
-            <div className="row">
-                <div className="col-12">
-                    <div className="news" >
-                        <div className="news-main" style={{ color: "#f5f5f5", backgroundColor: "#112B3C" }} >
-                            <div className="news-img"><img src="https://www.it.kmitl.ac.th/wp-content/uploads/2019/06/1300x867_BSC_TCAS-5-2562-ANNOUCEMENT-OF-QUALIFIED-CANDIDATES-FOR-INTERVIEW.jpg" width="100%" /></div>
-                            <div className="m-2" style={{ color: "#f5f5f5", backgroundColor: "#112B3C" }}>
-                                <div className="news-topic">{data[0].topic}</div>
-                                <span className="news-date">{data[0].date}</span>
-                                <div className="news-short">{data[0].short}</div>
-                            </div>
-                        </div>
-                        <div className="news-list" >
-                            <NewsItem news={data[0]} />
-                            <NewsItem news={data[0]} />
-                            <NewsItem news={data[0]} />
-                        </div>
-                    </div>
-                    <div className="text-center my-3">
-                        <Pagination size="small" total={50} />
-                    </div>
-                </div>
+            <div className="col-12 col-md-6">
+              <div className="news-img">
+                <img
+                  src="https://www.it.kmitl.ac.th/wp-content/uploads/2019/06/1300x867_BSC_TCAS-5-2562-ANNOUCEMENT-OF-QUALIFIED-CANDIDATES-FOR-INTERVIEW.jpg"
+                  width="100%"
+                />
+              </div>
             </div>
-
-            <div style={{ padding: '30px', paddingTop: '100px' }}>
-                <Typography.Title level={2}>
-                    ปฏิทินการศึกษา
-                </Typography.Title>
-                <CalendarItem lists={{}} />
-            </div>
-            {news?.map(_news => <>
-                <h2>
-                    {_news.title}
-                </h2>
-                <p>
-                    {_news.content}
-                </p>
-            </>)}
-
-            <div className="container text-center my-5">
-                <div className="row">
-                    <Typography.Title level={2} className="mb-4">
-                        เจ้าหน้าที่ห้องฟ้า
-                    </Typography.Title>
-                    <div className="col-4">
-                        <div className="mt-5">
-                            <Avatar size={200} />
-                        </div>
-                    </div>
-                    <div className="col-4">
-                        <div>
-                            <Avatar size={250} />
-                            <h4 className="mt-3">staff 1</h4>
-                            <hr></hr>
-                            <h6>contact : email</h6>
-                        </div>
-                    </div>
-                    <div className="col-4">
-                        <div className="mt-5">
-                            <Avatar size={200} />
-                        </div>
-                    </div>
-                </div>
-
-                
-
-            </div>
-
-
-            <div className="container text-center">
-                <div className="row">
-                    <Typography.Title level={2} className="mb-4">
-                        อาจารย์
-                    </Typography.Title>
-                    
-                </div>
-
-                
-
-            </div>
-
+          </div>
         </div>
-        {/* <div style={{ padding: '30px' }} div className="container"> */}
-        {/* <Typography.Title level={4}>
+      </Layout>
+
+      <div className="container my-5">
+        <Typography.Title level={2}>ข่าวสารและประชาสัมพันธ์</Typography.Title>
+        <div className="row">
+          <div className="col-12">
+            <div className="news">
+              <div
+                className="news-main"
+                style={{ color: "#f5f5f5", backgroundColor: "#112B3C" }}
+              >
+                <div className="news-img">
+                  <img
+                    src="https://www.it.kmitl.ac.th/wp-content/uploads/2019/06/1300x867_BSC_TCAS-5-2562-ANNOUCEMENT-OF-QUALIFIED-CANDIDATES-FOR-INTERVIEW.jpg"
+                    width="100%"
+                  />
+                </div>
+                <div
+                  className="m-2"
+                  style={{ color: "#f5f5f5", backgroundColor: "#112B3C" }}
+                >
+                  <div className="news-topic">{data[0].topic}</div>
+                  <span className="news-date">{data[0].date}</span>
+                  <div className="news-short">{data[0].short}</div>
+                </div>
+              </div>
+              <div className="news-list">
+                <NewsItem news={data[0]} />
+                <NewsItem news={data[0]} />
+                <NewsItem news={data[0]} />
+              </div>
+            </div>
+            <div className="text-center my-3">
+              <Pagination size="small" total={50} />
+            </div>
+          </div>
+        </div>
+
+        <div style={{ padding: "30px", paddingTop: "100px" }}>
+          <Typography.Title level={2}>ปฏิทินการศึกษา</Typography.Title>
+          <CalendarItem lists={{}} />
+        </div>
+        {news?.map((_news) => (
+          <>
+            <h2>{_news.title}</h2>
+            <p>{_news.content}</p>
+          </>
+        ))}
+
+        <div className="container text-center my-5">
+          <div className="row">
+            <Typography.Title level={2} className="mb-4">
+              เจ้าหน้าที่ห้องฟ้า
+            </Typography.Title>
+            {staff?.users?.map((s) => (
+              <div className="col-4">
+                <div className="mt-5">
+                  <Avatar size={200} src={<Image src={s.imageUri}   />} />
+                </div>
+                <h2>{s.fullname}</h2>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="container text-center">
+          <div className="row">
+            <Typography.Title level={2} className="mb-4">
+              อาจารย์
+            </Typography.Title>
+          </div>
+        </div>
+      </div>
+      {/* <div style={{ padding: '30px' }} div className="container"> */}
+      {/* <Typography.Title level={4}>
                 ข่าวสารและประชาสัมพันธ์
             </Typography.Title>
 
@@ -188,13 +204,13 @@ export const Home = memo(() => {
                 </div>
             </div> */}
 
-        {/* <Button type="primary" shape="round" size="large" style={{ marginTop: '30px', float: 'right' }} icon={<AlertFilled />} onClick={() => location.href = '/news'}>
+      {/* <Button type="primary" shape="round" size="large" style={{ marginTop: '30px', float: 'right' }} icon={<AlertFilled />} onClick={() => location.href = '/news'}>
                 <span className="thaiFont">
                     ข่าวสารเพิ่มเติม
                 </span>
             </Button> */}
 
-        {/* <div style={{ padding: '30px', paddingTop: '100px' }}>
+      {/* <div style={{ padding: '30px', paddingTop: '100px' }}>
                 <Typography.Title level={3}>
                     ปฏิทินการศึกษา
                 </Typography.Title>
@@ -208,8 +224,7 @@ export const Home = memo(() => {
                     {_news.content}
                 </p>
             </>)} */}
-        {/* </div> */}
-
+      {/* </div> */}
     </>
-
+  );
 });
