b53a8b6404f46efd188f5dcc7851aa16805184db
Yannawut62070051 <56510625+Yannawut62070051@users.noreply.github.com>
2022-05-24 00:00:37 +0700

    change modal style

--- frontend/src/pages/Service.jsx
+++ frontend/src/pages/Service.jsx
@@ -1,16 +1,26 @@
 import { memo, useEffect, useCallback, useState } from "react";
-import { Row, Col, Card, List, Typography, Divider, Tabs, Modal, Layout } from 'antd';
-import { PlusCircleFilled } from '@ant-design/icons';
+import {
+  Row,
+  Col,
+  Card,
+  List,
+  Typography,
+  Divider,
+  Tabs,
+  Modal,
+  Layout,
+} from "antd";
+import { PlusCircleFilled } from "@ant-design/icons";
 import { Link } from "react-router-dom";
-import { Spin, Space } from 'antd';
-import { Button, Popover } from 'antd';
+import { Spin, Space } from "antd";
+import { Button, Popover } from "antd";
 
-import requestTopics from "../data/requestInfo.json"
-import requests from "../data/requestItems.json"
+import requestTopics from "../data/requestInfo.json";
+import requests from "../data/requestItems.json";
 import RequestInfo from "../Data/ResquestInfo";
 import { RequestTopic } from "../components/Services/RequestTopic";
 import { RequestList } from "../components/Services/RequestList";
-import { gql, useQuery, useMutation } from "@apollo/client"
+import { gql, useQuery, useMutation } from "@apollo/client";
 const { TabPane } = Tabs;
 // const data = [
 //     'Racing car sprays burning fuel into crowd.',
@@ -23,94 +33,101 @@ const { TabPane } = Tabs;
 const { Meta } = Card;
 
 const queryService = gql`
-query {
+  query {
     generalRequest {
-      _id,
-      title,
-      status,
-      description,
-    },
-    leaveRequest {
-      _id,
-      title,
-      status,
+      _id
+      title
+      status
       description
-      leaveType,
     }
-  }`;
+    leaveRequest {
+      _id
+      title
+      status
+      description
+      leaveType
+    }
+  }
+`;
 
 export const Service = memo(() => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [items, setItems] = useState([...RequestInfo]);
+  const [isError, setIsError] = useState(false);
 
-    const [isModalVisible, setIsModalVisible] = useState(false);
-    const [items, setItems] = useState([...RequestInfo])
-    const [isError, setIsError] = useState(false)
+  const [requestPopup, setRequestPopup] = useState(null);
 
-    const [requestPopup, setRequestPopup] = useState(null)
+  const { data: dataService, refetch } = useQuery(queryService);
 
-    const { data: dataService, refetch } = useQuery(queryService);
+  const showModal = useCallback(
+    (req) => () => {
+      setRequestPopup(req);
+      setIsModalVisible(true);
+    },
+    []
+  );
 
-    const showModal = useCallback(
-        (req) => () => {
-            setRequestPopup(req)
-            setIsModalVisible(true);
-        },
-        []
-    )
+  const handleCreate = useCallback(
+    (page) => () => {
+      location.href = `service/${page}/create`;
+      setIsModalVisible(false);
+    },
+    []
+  );
 
-    const handleCreate = useCallback(
-        (page) => () => {
-            location.href = `service/${page}/create`
-            setIsModalVisible(false);
-        },
-        []
-    )
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
 
-    const handleCancel = () => {
-        setIsModalVisible(false);
-    };
+  if (isError)
+    return (
+      <>
+        <div>
+          <Spin size="large" />
+        </div>
+      </>
+    );
 
-    if (isError)
-        return <>
-            <div>
-                <Spin size="large" />
-            </div>
-        </>
-
-
-    return <>
-        <Layout style={{ background: "linear-gradient(180deg, #112B3C 70%, rgb(235, 245, 255) 50%)" }}>
-            <div className="container my-5 text-center">
-                <div className="row mb-2">
-                    <Typography.Title level={3} style={{ color: "#d9d9d9" }}>
-                        ประเภทเอกสาร
-                    </Typography.Title>
-                    <p style={{ color: "#d9d9d9" }}>นักศึกษาสร้างเอกสาร บลาๆๆๆ ตามขั้นตอนต่อไปนี้ (คำอธิบาย step การยื่นภาพรวม)</p>
+  return (
+    <>
+      <Layout
+        style={{
+          background:
+            "linear-gradient(180deg, #112B3C 70%, rgb(235, 245, 255) 50%)",
+        }}
+      >
+        <div className="container my-5 text-center">
+          <div className="row mb-2">
+            <Typography.Title level={3} style={{ color: "#d9d9d9" }}>
+              ประเภทเอกสาร
+            </Typography.Title>
+            <p style={{ color: "#d9d9d9" }}>
+              ประเภทเอกสารที่นักศึกษาต้องการจะส่งพิจารณา
+            </p>
+          </div>
+          <div className="row mb-0">
+            {requestTopics.map((request) => (
+              <>
+                <div className="col-2 col-md-2 mx-5">
+                  <RequestTopic request={request} toggleModal={showModal} />
                 </div>
-                <div className="row mb-0">
-                    {
-                        requestTopics.map((request) => (
-                            <>
-                                <div className="col-2 col-md-2 mx-5">
-                                    <RequestTopic request={request} toggleModal={showModal} />
-
-                                </div>
-                            </>
-                        ))
-                    }
-                </div>
-            </div>
-        </Layout>
-        <div className="container ">
-            <div className="request-body row justify-content-center">
-                <Tabs defaultActiveKey="0">
-                    <TabPane tab="ทั้งหมด" key="0" className="request-list ">
-                        {
-                            [...dataService?.generalRequest ?? [] , ...dataService?.leaveRequest ?? []].map((req) => (
-                                <RequestList request={req} />
-                            ))
-                        }
-                    </TabPane>
-                    {/* {
+              </>
+            ))}
+          </div>
+        </div>
+      </Layout>
+      <div className="container ">
+        <div className="request-body row justify-content-center">
+          <Tabs defaultActiveKey="0">
+            <TabPane tab="ทั้งหมด" key="0" className="request-list ">
+              {[
+                ...(dataService?.generalRequest ?? []),
+                ...(dataService?.leaveRequest ?? []),
+              ].map((req) => (
+                <RequestList request={req} />
+              ))}
+            </TabPane>
+            {/* {
                         requestTopics.map((topic, index) => (
                             <TabPane tab={topic.name} key={index + 1} className="request-list">
                                 {
@@ -121,54 +138,59 @@ export const Service = memo(() => {
                             </TabPane>
                         ))
                     } */}
-                    <TabPane tab="ใบคำร้องทั่วไป" key="1" className="request-list">
-                        {
-                            dataService?.generalRequest?.map((req) => (
-                                <RequestList request={req} />
-                            ))
-                        }
-
-                    </TabPane>
-                    <TabPane tab="ใบลาป่วย" key="2" className="request-list">
-                        {
-                            dataService?.leaveRequest?.filter(l => l.leaveType === "Sick").map((req) => (
-                                <RequestList request={req} />
-                            ))
-                        }
-                    </TabPane>
-                    <TabPane tab="ใบลากิจ" key="3" className="request-list">
-                        {
-                            dataService?.leaveRequest?.filter(l => l.leaveType === "Business").map((req) => (
-                                <RequestList request={req} />
-                            ))
-                        }
-                    </TabPane>
-                    <TabPane tab="ใบทุนการศึกษา" key="ภ" className="request-list">
-                        
-                    </TabPane>
-                </Tabs>
-            </div>
-
-            {
-                requestPopup ?
-
-                    <Modal
-                        title={requestPopup.name}
-                        visible={isModalVisible}
-                        onOk={handleCreate(requestPopup.page)}
-                        onCancel={handleCancel}
-                        okText="สร้างเอกสาร"
-                    >
-                        <h2 className="text-center mb-3 fs-3">{requestPopup.name}</h2>
-                        <h6>ประเภท : {requestPopup.category.join(" ")}</h6>
-                        <h6>รายละเอียด : {requestPopup.description}</h6>
-                    </Modal> : ""
-            }
-
-
-
+            <TabPane tab="ใบคำร้องทั่วไป" key="1" className="request-list">
+              {dataService?.generalRequest?.map((req) => (
+                <RequestList request={req} />
+              ))}
+            </TabPane>
+            <TabPane tab="ใบลาป่วย" key="2" className="request-list">
+              {dataService?.leaveRequest
+                ?.filter((l) => l.leaveType === "Sick")
+                .map((req) => (
+                  <RequestList request={req} />
+                ))}
+            </TabPane>
+            <TabPane tab="ใบลากิจ" key="3" className="request-list">
+              {dataService?.leaveRequest
+                ?.filter((l) => l.leaveType === "Business")
+                .map((req) => (
+                  <RequestList request={req} />
+                ))}
+            </TabPane>
+            <TabPane
+              tab="ใบทุนการศึกษา"
+              key="4"
+              className="request-list"
+            ></TabPane>
+          </Tabs>
         </div>
 
+        {requestPopup ? (
+          <Modal
+            className="p-5"
+            visible={isModalVisible}
+            //onOk={this.handleOk}
+            onCancel={handleCancel}
+            footer={null}
+          >
+            <h2 className="text-center mb-3 fs-3">{requestPopup.name}</h2>
+            <h6>ตัวอย่างเช่น {requestPopup.category.join(" ")}</h6>
+            <h6>รายละเอียด : {requestPopup.description}</h6>
+            <div class="row">
+              <Button
+                className="mt-4"
+                type="primary"
+                size="large"
+                onClick={handleCreate(requestPopup.page)}
+              >
+                สร้างเอกสาร
+              </Button>
+            </div>
+          </Modal>
+        ) : (
+          ""
+        )}
+      </div>
     </>
-
+  );
 });
